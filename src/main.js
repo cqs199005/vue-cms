569534e41be5bb83f8647162ddba8b37bb8f1eaa
江湖小虾米 <366797700@qq.com>
2018-05-08 22:31:36 +0800

    5.8日提交

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@
 //引入Vue模块
 import Vue from "vue"
 
-
+//导入并安装路由模块
+import vueRouter from "vue-router"
+Vue.use(vueRouter)
+//导入自定义路由文件
+import router from "./router.js"
 
 
 //按需导入MintUI组件
@@ -12,7 +16,7 @@ Vue.component(Header.name, Header);
 
 //导入mui框架的样式文件
 import "./lib/mui/css/mui.min.css"
-
+import "./lib/mui/css/icons-extra.css"
 
 
 //导入App模块
@@ -21,5 +25,6 @@ import app from "./App.vue"
 //创建Vue实例
 var vm = new Vue({
     el:"#app",
-    render:c=>c(app)  //渲染组件
+    render:c=>c(app),  //渲染组件
+    router,  //注册路由对象
 })
--- src/router.js
+++ src/router.js
@@ -1,10 +1,21 @@
 import VueRouter from 'vue-router'
 
+//导入路由组件
+import home from "./components/tapbar/home.vue"
+import member from "./components/tapbar/member.vue"
+import search from "./components/tapbar/search.vue"
+import shopcar from "./components/tapbar/shopcar.vue"
+
 
 // 3. 创建路由对象
 var router = new VueRouter({
   routes: [
-  ]
+    {path:"/home",component:home},
+    {path:"/member",component:member},
+    {path:"/shopcar",component:shopcar},
+    {path:"/search",component:search},
+  ],
+  linkActiveClass:"mui-active" //修改路由被选中状态默认添加的类名
 })
 
 // 把路由对象暴露出去
